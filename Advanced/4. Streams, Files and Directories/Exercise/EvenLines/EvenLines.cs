7f1495b4f4028b3d909d6ed47bda17a13a7a8e8e
Ekrem05 <ekremnptg@gmail.com>
2023-01-23 20:06:01 +0200

    done

---
+++ Advanced/4. Streams, Files and Directories/Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,60 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+             StringBuilder sb = new();
+            using (StreamReader reader= new(inputFilePath))
+            {
+               
+                string line = reader.ReadLine();
+                int lineCounter = 0;
+                while (line!=null)
+                {
+                    if (lineCounter%2==0)
+                    {
+                                              
+                        line = ReplaceSymbols(line);
+                        line = ReverseLine(line);
+                        sb.AppendLine(line);
+                    }
+                    line = reader.ReadLine();
+                    lineCounter++;
+                }
+            }
+            return sb.ToString();
+        }
+
+       
+
+        private static string ReplaceSymbols(string line)
+        {
+            StringBuilder sb = new(line);
+            string[] symbolsForReplacing = { "-", ",", ".", "!","?" };
+           
+            foreach (var symbol in symbolsForReplacing)
+            {
+                sb.Replace(symbol, "@");
+            }
+            return sb.ToString();
+        }
+         private static string ReverseLine(string line)
+        {
+            string[] split = line.Split(" ").Reverse().ToArray();
+            return string.Join(" ", split);
+         }
+     }
+}
---
+++ Advanced/4. Streams, Files and Directories/Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,47 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder stringBuilder = new();
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int lineCounter = 0;
+            foreach (var line in lines)
+            {  
+                 lineCounter++;
+                int punctuationCount = 0;
+                int wordsCount = 0;
+                char[]chars= line.ToCharArray();
+                foreach (var item in chars)
+                {
+                    if (Char.IsPunctuation(item))
+                    {
+                        punctuationCount++;
+                    }
+                    else if (Char.IsLetter(item))
+                    {
+                        wordsCount++;
+                    }
+                }
+                stringBuilder.AppendLine($"Line {lineCounter}: {line} ({wordsCount})({punctuationCount})");
+             
+                
+
+            }  
+            File.AppendAllText(outputFilePath, stringBuilder.ToString());
+        }
+    }
+}
