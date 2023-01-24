b3f30b37b52356498c1acf4ffdab6796c638a93f
Ekrem05 <ekremnptg@gmail.com>
2023-01-24 21:56:40 +0200

    .

---
+++ Advanced/5. Functional Programming/Main/1. Sort Even Numbers/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] array = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(x => x % 2 == 0)
+    .OrderBy(x=>x)
+    .ToArray();
+Console.WriteLine(string.Join(", ",array));
---
+++ Advanced/5. Functional Programming/Main/2. Sum Numbers/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] array = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+Console.WriteLine(array.Count());
+Console.WriteLine(array.Sum());
