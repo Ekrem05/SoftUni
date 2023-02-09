781489316d40e081f76c2350116ad464cf3a278f
Ekrem05 <ekremnptg@gmail.com>
2023-02-09 20:07:26 +0200

    100

---
+++ Advanced/10. Iterators and Comparators/IteratorsAndComparators/IteratorsAndComparators/LibraryIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        int currentIndex = -1;
+
+        public LibraryIterator(List<Book> books)
+        {
+            
+            this.books = new List<Book>(books);
+        }
+
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+        
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex =-1;
+        }
+    }
+}
