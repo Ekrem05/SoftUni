c3be2cda17cfe8f884c7a6de92d70cf80912d1b5
Ekrem05 <ekremnptg@gmail.com>
2023-02-13 19:45:30 +0200

    100

---
+++ Advanced/11. Exam Preparation/TilesMaster/TilesMaster/Program.cs
@@ -0,0 +1,88 @@
+﻿Stack<int>greyTiles= new Stack<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+Queue<int> whiteTiles = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+Dictionary<string,int>tiles= new();
+tiles.Add("Sink", 40);
+tiles.Add("Oven", 50);
+tiles.Add("Countertop", 60);
+tiles.Add("Wall", 70);
+List<int>areas= new List<int>() {40,50,60,70};
+Dictionary<string, int> tilesForUse = new();
+while (true)
+{
+	int currentWhiteTile = whiteTiles.Peek();
+    int currentGreyTile = greyTiles.Peek();
+    if (currentGreyTile==currentWhiteTile)
+	{
+		int largeTile = currentWhiteTile + currentGreyTile;
+		if (areas.Contains(largeTile))
+		{
+			greyTiles.Pop();
+			whiteTiles.Dequeue();
+			foreach (var location in tiles)
+			{
+				if (location.Value==largeTile)
+				{
+					if (!tilesForUse.ContainsKey(location.Key))
+					{
+						tilesForUse.Add(location.Key, 1);
+						break;
+					}
+					else
+					{
+						tilesForUse[location.Key]++;
+
+                            break;
+                    }
+				}
+			}
+		}
+		else
+		{
+            if (!tilesForUse.ContainsKey("Floor"))
+            {
+                tilesForUse.Add("Floor", 1);
+                
+            }
+            else
+            {
+                tilesForUse["Floor"]++;
+
+               
+            }
+            greyTiles.Pop();
+            whiteTiles.Dequeue();
+        }
+	}
+	else
+	{
+		currentWhiteTile /=  2;
+		whiteTiles.Dequeue();
+		whiteTiles.Enqueue(currentGreyTile);
+		greyTiles.Pop();
+		greyTiles.Push(currentGreyTile);
+	}
+	if (!greyTiles.Any()||!whiteTiles.Any())
+	{
+		break;
+	}
+}
+if (whiteTiles.Any())
+{
+	Console.WriteLine("White tiles left: "+string.Join(", ",whiteTiles));
+}
+else
+{
+    Console.WriteLine("White tiles left: none");
+}
+if (greyTiles.Any())
+{
+    Console.WriteLine("Grey tiles left: " + string.Join(", ", greyTiles));
+}
+else
+{
+    Console.WriteLine("Grey tiles left: none");
+}
+foreach (var item in tilesForUse.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+{
+	Console.WriteLine($"{item.Key}: {item.Value}");
+}
---
+++ Advanced/11. Exam Preparation/TilesMaster/WallDestroyer/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallDestroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+           string[,] wall = new string[size, size];
+            int row = -1;
+            int col = -1;
+            int holes = 0;
+            int steelRods = 0;
+            for (int i = 0; i < size; i++)
+            {
+                string input = Console.ReadLine();
+                for (int rows = 0; rows < size; rows++)
+                {
+                    wall[i, rows] = input[rows].ToString();
+                    if (wall[i, rows] == "V")
+                    {
+                        row = i;
+                        col = rows;
+                        wall[i, rows] = "*";
+                        holes++;
+                    }
+                }
+            }
+            string move = string.Empty;
+            bool electrocuted = false;
+            while ((move = Console.ReadLine()) != "End")
+            {
+                switch (move)
+                {
+                    case "up":
+                        row--;
+                        break;
+                    case "down":
+                        row++;
+                        break;
+                    case "right":
+                        col++;
+                        break;
+                    case "left":
+                        col--;
+                        break;
+
+                }
+                if (row>=0&&col>=0&&row<wall.GetLength(0)&&col<wall.GetLength(1))
+                {
+                    if (wall[row, col] == "R")
+                {
+                    switch (move)
+                    {
+                        case "up":
+                            row++;
+                            break;
+                        case "down":
+                            row--;
+                            break;
+                        case "right":
+                            col--;
+                            break;
+                        case "left":
+                            col++;
+                            break;
+
+                    }
+                    Console.WriteLine("Vanko hit a rod!");
+                    steelRods++;
+                }
+                else if (wall[row, col] == "C")
+                {
+                    wall[row, col] = "E";
+                    holes++;
+                    electrocuted = true;
+                    break;
+                }
+                else if (wall[row, col] == "*")
+                {
+                    Console.WriteLine($"The wall is already destroyed at position [{row}, {col}]!");
+                }
+                else
+                {
+                    wall[row, col] = "*";
+                    holes++;
+                }
+                }
+                else
+                {
+                    switch (move)
+                    {
+                        case "up":
+                            row++;
+                            break;
+                        case "down":
+                            row--;
+                            break;
+                        case "right":
+                            col--;
+                            break;
+                        case "left":
+                            col++;
+                            break;
+
+                    }
+                }
+                
+            }
+            if (!electrocuted)
+            {
+                Console.WriteLine($"Vanko managed to make {holes} hole(s) and he hit only {steelRods} rod(s).");
+            }
+            else
+            {
+                Console.WriteLine($"Vanko got electrocuted, but he managed to make {holes} hole(s).");
+
+
+            }
+            if (wall[row, col] != "E")
+            {
+                wall[row, col] = "V";
+            }
+            PrintMatrix(wall);
+
+
+        }
+
+        public static void PrintMatrix(string[,] battleField)
+        {
+            for (int i = 0; i < battleField.GetLength(0); i++)
+            {
+                for (int k = 0; k < battleField.GetLength(1); k++)
+                {
+                    Console.Write(battleField[i, k]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    
+}
+
