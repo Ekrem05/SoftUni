8fa04f629caa4d59413906873ba3a28efbe0c8a3
Ekrem05 <ekremnptg@gmail.com>
2023-03-09 19:52:24 +0200

    100

---
+++ OOP/6. Debugging Techniques/DebugTest/DebugTest/Start.cs
@@ -0,0 +1,12 @@
+﻿namespace DebugTest
+{
+    internal class Start
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add(Console.ReadLine());
+            Console.WriteLine(list[0]);
+        }
+    }
+}
