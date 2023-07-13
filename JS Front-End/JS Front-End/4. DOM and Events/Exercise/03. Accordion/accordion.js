e55131f4496f2999ecfc721c7a94cb87c246e739
Ekrem05 <ekremnptg@gmail.com>
2023-07-13 20:06:37 +0300

    DOM excersice

--- JS Front-End/JS Front-End/4. DOM and Events/Exercise/03. Accordion/accordion.js
+++ JS Front-End/JS Front-End/4. DOM and Events/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+  const button = document.getElementsByClassName("button")[0];
+  if (button.textContent === "More") {
+    button.textContent = "Less";
+    document.getElementById("extra").style.display = "block";
+  } else {
+    button.textContent = "More";
+    document.getElementById("extra").style.display = "none";
+  }
 }
--- JS Front-End/JS Front-End/4. DOM and Events/Exercise/04. Locked-Profile/app.js
+++ JS Front-End/JS Front-End/4. DOM and Events/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const arrayOfButtons = Array.from(document.querySelectorAll("button"));
+  arrayOfButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const parent = button.parentElement;
+      const hidden = parent.children[9];
+      const locked = parent.children[4];
+      if (button.textContent == "Show more") {
+        if (locked.checked == true) {
+          hidden.style.display = "block";
+          button.textContent = "Hide it";
+        }
+      } else {
+        if (locked.checked == true) {
+          hidden.style.display = "none";
+          button.textContent = "Show more";
+        }
+      }
+    });
+  });
 }
