a87b20ec329a67a1ae7a7fd060206e42ab52fbfe
Ekrem05 <ekremnptg@gmail.com>
2023-09-18 19:57:52 +0300

    asd

--- C# DB/MS SQL/CRUD/SQLQuery2.sql
+++ C# DB/MS SQL/CRUD/SQLQuery2.sql
@@ -19,3 +19,15 @@ from Employees
 Select *
 from Employees
 Where JobTitle = 'Sales Representative'
+
+Select FirstName, LastName, JobTitle
+from Employees
+Where Salary>=20000 and Salary<=30000
+
+Select Concat(FirstName,' ', MiddleName, ' ',LastName)
+from Employees
+Where Salary=25000 or Salary='14000' or Salary='12500 'or Salary='23600'
+
+Select FirstName, LastName
+from Employees
+Where ManagerID is Null
