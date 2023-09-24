038161b735511b0f4e34d209ab027fbfe03f9669
Ekrem05 <ekremnptg@gmail.com>
2023-09-24 18:37:43 +0300

    /.

--- C# DB/MS SQL/Built-In-Functions/SQLQuery1.sql
+++ C# DB/MS SQL/Built-In-Functions/SQLQuery1.sql
@@ -5,3 +5,7 @@ Where FirstName like 'Sa_%'
 Select FirstName, LastName
 from Employees
 Where LastName like '%_ei_%'
+
+Select FirstName
+from Employees
+Where DepartmentID=3 or DepartmentID=10 and YEAR(HireDate)>1995 and YEAR(HireDate)<2005
