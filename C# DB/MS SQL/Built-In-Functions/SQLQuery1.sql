776b6fd34c5604ec66600a055062d229841eb067
Ekrem05 <ekremnptg@gmail.com>
2023-09-24 18:50:53 +0300

    /

--- C# DB/MS SQL/Built-In-Functions/SQLQuery1.sql
+++ C# DB/MS SQL/Built-In-Functions/SQLQuery1.sql
@@ -9,3 +9,17 @@ Where LastName like '%_ei_%'
 Select FirstName
 from Employees
 Where DepartmentID=3 or DepartmentID=10 and YEAR(HireDate)>1995 and YEAR(HireDate)<2005
+
+Select FirstName,LastName,UPPER(JobTitle)
+from Employees
+Where UPPER(JobTitle) not like'%ENGINEER%'
+
+Select [Name]
+from Towns
+Where DATALENGTH([Name])=5 or DATALENGTH([Name])=6
+Order by [Name]
+
+Select TownID, [Name]
+from Towns
+WHERE [Name] LIKE 'M%' OR [Name] LIKE 'K%' OR [Name] LIKE 'B%' OR [Name] LIKE 'E%'
+Order by [Name]
