b9bb18c025a37a6b7357702baacd117ffc4113d0
Ekrem05 <ekremnptg@gmail.com>
2023-09-24 19:15:02 +0300

    .

--- C# DB/MS SQL/Built-In-Functions/SQLQuery2.sql
+++ C# DB/MS SQL/Built-In-Functions/SQLQuery2.sql
@@ -6,3 +6,11 @@ Where YEAR(HireDate)>2000
 Select FirstName,LastName
 from Employees
 Where DATALENGTH(LastName)=5
+
+Select EmployeeID, FirstName,LastName, Salary, DENSE_RANK() OVER( PARTITION BY Salary ORDER BY EmployeeID asc) as 'Rank'
+from Employees
+Where Salary BETWEEN 10000 AND 50000
+Order by
+Salary
+desc
+
