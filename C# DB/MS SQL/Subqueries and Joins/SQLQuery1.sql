7331ff49ed6529f8dfa94b334b1c2aa726de3f07
Ekrem05 <ekremnptg@gmail.com>
2023-09-28 19:36:25 +0300

    .

--- C# DB/MS SQL/Subqueries and Joins/SQLQuery1.sql
+++ C# DB/MS SQL/Subqueries and Joins/SQLQuery1.sql
@@ -21,3 +21,23 @@ on e.DepartmentID=d.DepartmentID
 Where d.Name='Sales'
 Order by EmployeeID asc
 
+Select top 5 EmployeeID,FirstName,Salary,d.Name as DepartmentName
+from Employees as e
+Join Departments as d
+on e.DepartmentID=d.DepartmentID
+Where e.Salary>15000
+Order by e.DepartmentID asc
+
+Select top 3 e.EmployeeID,FirstName
+from Employees as e
+Join EmployeesProjects as ep
+on e.EmployeeID=ep.EmployeeID
+Where ep.ProjectID=null
+Order by e.EmployeeID asc
+
+Select FirstName,LastName,HireDate,d.[Name] as DeptName
+from Employees as e
+Join Departments as d
+on e.DepartmentID=d.DepartmentID
+Where e.HireDate>'1/1/1999' AND d.[Name]='Sales' or d.[Name]='Finance'
+Order by e.HireDate asc
