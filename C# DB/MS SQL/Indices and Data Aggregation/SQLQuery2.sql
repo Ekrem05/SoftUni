386c74e59e61ad87801707d653e9c62be81bb78c
Ekrem05 <ekremnptg@gmail.com>
2023-10-04 15:12:24 +0300

    .

--- C# DB/MS SQL/Indices and Data Aggregation/SQLQuery2.sql
+++ C# DB/MS SQL/Indices and Data Aggregation/SQLQuery2.sql
@@ -26,3 +26,22 @@ Where MagicWandCreator='Ollivander family'
 Group by DepositGroup
 Having Sum(DepositAmount)<150000
 Order BY Sum(DepositAmount) Desc
+
+SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) as MinDepositCharge
+FROM WizzardDeposits
+Group by DepositGroup, MagicWandCreator
+ORDER BY MagicWandCreator, DepositGroup
+
+SELECT AgeGroups, Count(*) as [WizzardCount] FROM(
+	SELECT Age,
+		Case
+		WHEN Age between 0 and 10 then '[0-10]'
+		WHEN Age between 11 and 20 then '[11-20]'
+		WHEN Age between 21 and 30 then '[21-30]'
+		WHEN Age between 31 and 40 then '[31-40]'
+		WHEN Age between 41 and 50 then '[41-50]'
+		WHEN Age between 51 and 60 then '[51-60]'
+		ELSE '[61+]'
+		END as AgeGroups
+	FROM WizzardDeposits) AS ASD
+Group by AgeGroups
