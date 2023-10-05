727b5bcce8d41460df231c8582d12e067091e41a
Ekrem05 <ekremnptg@gmail.com>
2023-10-05 18:45:53 +0300

    FUNC, PROC

--- C# DB/MS SQL/Functions and Stored Procedures/SQLQuery4.sql
+++ C# DB/MS SQL/Functions and Stored Procedures/SQLQuery4.sql
@@ -1,9 +1,58 @@
 CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
 AS
-	Select FirstName as [First Name], LastName as [Last Name]
+	Select FirstName, LastName
 	from Employees
 	Where Salary>35000
 	
 GO
 
 EXEC usp_GetEmployeesSalaryAbove35000
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber(@number Decimal(18,4))
+AS
+	Select FirstName, LastName
+	from Employees
+	Where Salary>=@number
+	
+GO
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
+
+CREATE OR ALTER PROCEDURE usp_GetTownsStartingWith @letter nvarchar(50)
+
+AS
+	Select	[Name] as Town
+	from Towns
+	Where [Name] like CONCAT(@letter,'%')
+	
+GO
+EXEC usp_GetTownsStartingWith 'B'
+
+
+CREATE Procedure usp_GetEmployeesFromTown @townName nvarchar(50)
+
+AS
+	Select FirstName, LastName
+	from Employees as e
+	JOIN Addresses as a
+	On e.AddressID=a.AddressID
+	JOIN Towns as t
+	On a.TownID=t.TownID
+	Where t.Name= @townName
+
+EXEC usp_GetEmployeesFromTown 'Berlin'
+
+CREATE function ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS varchar(10)
+	AS
+	Begin
+		DECLARE @result varchar(10)
+		IF @salary < 30000
+        SET @result = 'Low'
+    ELSE IF @salary >= 30000 AND @salary <= 50000
+        SET @result = 'Average'
+    ELSE
+        SET @result = 'High'
+
+    RETURN @result
+	END
+	
