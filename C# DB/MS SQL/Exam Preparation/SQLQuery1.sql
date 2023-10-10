ad6112eeaac5e57d07d0d56afdc884f1f5af05b8
Ekrem05 <ekremnptg@gmail.com>
2023-10-10 18:38:39 +0300

    EXAM PREP

--- C# DB/MS SQL/Exam Preparation/SQLQuery1.sql
+++ C# DB/MS SQL/Exam Preparation/SQLQuery1.sql
@@ -1,23 +1,24 @@
 CREATE DATABASE Boardgames
 
-Create TABLE Addresses(
-Id INT PRIMARY KEY IDENTITY(1,1),
-StreetName varchar(100) NOT NULL,
-StreetNumber varchar(100) NOT NULL,
-Town varchar(30) NOT NULL,
-Country varchar(50) NOT NULL,
-ZIP INT NOT NULL,
-)
 
 Create TABLE Categories(
 Id INT PRIMARY KEY IDENTITY(1,1),
 Name varchar(50) NOT NULL,
 
 )
+
+Create TABLE Addresses(
+Id INT PRIMARY KEY IDENTITY(1,1),
+StreetName varchar(100) NOT NULL,
+StreetNumber Int NOT NULL,
+Town varchar(30) NOT NULL,
+Country varchar(50) NOT NULL,
+ZIP INT NOT NULL,
+)
 Create TABLE Publishers(
 Id INT PRIMARY KEY IDENTITY(1,1),
 Name varchar(30) UNIQUE NOT NULL,
-AddressId INT Foreign Key References Addresses(Id),
+AddressId INT Foreign KEY REFERENCES Addresses(Id),
 Website varchar(40) NULL,
 Phone varchar(20)  NULL,
 )
@@ -31,9 +32,9 @@ PlayersMax int NOT NULL,
 
 Create TABLE Boardgames(
 Id INT PRIMARY KEY IDENTITY(1,1),
-Name varchar(30)  NOT NULL,
+Name nvarchar(30)  NOT NULL,
 YearPublished int NOT NULL,
-Rating DECIMAL(18,4) NOT NULL,
+Rating DECIMAL(4,2) NOT NULL,
 CategoryId INT Foreign Key References Categories(Id),
 PublisherId INT Foreign Key References Publishers(Id),
 PlayersRangeId INT Foreign Key References PlayersRanges(Id),
@@ -41,13 +42,13 @@ PlayersRangeId INT Foreign Key References PlayersRanges(Id),
 
 Create TABLE Creators(
 Id INT PRIMARY KEY IDENTITY(1,1),
-FirstName varchar(30)  NOT NULL,
-LastName varchar(30)  NOT NULL,
-Email varchar(30)  NOT NULL,
+FirstName nvarchar(30)  NOT NULL,
+LastName nvarchar(30)  NOT NULL,
+Email nvarchar(30)  NOT NULL,
 
 )
 Create TABLE CreatorsBoardgames(
 CreatorId INT Foreign Key References Creators(Id),
 BoardgameId INT Foreign Key References Boardgames(Id),
-
+PRIMARY KEY(CreatorId,BoardgameId)
 )
