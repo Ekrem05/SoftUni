823b16deae44b4a83896ad1e9b3ebac5cfeb11a7
Ekrem05 <ekremnptg@gmail.com>
2023-10-18 15:39:19 +0300

    ADO.NET

---
+++ C# DB/Entity Framework Core/ADO.NET/ADO.NET/ADO.NET/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection con =
+                new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=MinionsDB;");
+            using (con)
+            {
+                con.Open();
+                string querry = "Select [Name], Count(*)as Count from Villains as v Join MinionsVillains as mv ON mv.VillainId=v.Id Group By [Name] HAVING Count(*)>3 Order By Count(*) desc";
+                SqlCommand command = new SqlCommand(querry,con);
+                SqlDataReader dataReader = command.ExecuteReader();
+                while ( dataReader.Read())
+                {
+                    Console.WriteLine(dataReader[0].ToString() +" - "+ dataReader[1].ToString());
+                }
+            }
+        }
+    }
+}
