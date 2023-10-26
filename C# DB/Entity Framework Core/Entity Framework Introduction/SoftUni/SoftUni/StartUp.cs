0b0475523dee1c073521282c9eeecd0a3f68569e
Ekrem05 <ekremnptg@gmail.com>
2023-10-26 22:30:28 +0300

    EF

---
+++ C# DB/Entity Framework Core/Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetEmployeesFullInformation(new SoftUniContext()) ); 
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+           var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.MiddleName,
+                    e.LastName,
+                    e.Salary,
+                    e.HireDate
+                }).ToList();
+
+            StringBuilder sb = new();
+            sb.Append(string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} {e.MiddleName} {e.LastName} {e.Salary:f2} {e.HireDate}")));
+            return sb.ToString();
+        }
+    }
+}
