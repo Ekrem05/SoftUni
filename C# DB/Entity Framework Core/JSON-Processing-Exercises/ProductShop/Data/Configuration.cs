67d8c1f8b1e1a84a8951157f490d55bc68111ae1
Ekrem05 <ekremnptg@gmail.com>
2023-11-14 13:50:24 +0200

    0.

--- C# DB/Entity Framework Core/JSON-Processing-Exercises/ProductShop/Data/Configuration.cs
+++ C# DB/Entity Framework Core/JSON-Processing-Exercises/ProductShop/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public const string ConnectionString =
-            @"Server=.;Database=ProductShop;Integrated Security=True";
+            @"Server=(localdb)\\MSSQLLocalDB;Database=ProductShop;Integrated Security=True";
     }
 }
