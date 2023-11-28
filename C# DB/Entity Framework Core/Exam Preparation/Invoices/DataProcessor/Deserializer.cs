3b9e28c4b359cca26a25545410f4759eb57a7564
Ekrem05 <ekremnptg@gmail.com>
2023-11-28 19:32:16 +0200

    .

--- C# DB/Entity Framework Core/Exam Preparation/Invoices/DataProcessor/Deserializer.cs
+++ C# DB/Entity Framework Core/Exam Preparation/Invoices/DataProcessor/Deserializer.cs
@@ -29,7 +29,8 @@
             {
                 if (!IsValid(client))
                 {
-
+                    sb.AppendLine(ErrorMessage);
+                    continue;
                 }
             }
 
